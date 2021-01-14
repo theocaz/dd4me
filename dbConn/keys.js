f099884848f4dcf43fd92c519e6d4537d851746d
Theo <theocaz@gmail.com>
2021-01-13 19:22:25 -0500

    heroku dbconn

--- dbConn/keys.js
+++ dbConn/keys.js
@@ -1,10 +1,10 @@
 // put in your db connection settings and rename to 'keys.js'
 module.exports = {
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
+  host: "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "bv36byf4elf4ylic",
+  password: "ksj049y94e828tdq",
   connectionLimit: 5,
-  database: "dd4me"
+  database: "u3yikfybcx1baovi"
 };
 
 /*
