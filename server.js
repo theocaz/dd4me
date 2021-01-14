bc9339c3d0246c1458653a64aa2c8b860495eed3
Theo <theocaz@gmail.com>
2021-01-13 19:12:12 -0500

    npm update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 9000
 const axios = require('axios');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
