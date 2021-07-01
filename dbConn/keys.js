dd57c65b90f2b180d8417fa83e51f987583f8362
Theo <theocaz@gmail.com>
2021-07-01 19:58:00 -0400

    Update

--- dbConn/keys.js
+++ dbConn/keys.js
@@ -1,11 +1,11 @@
 // db keys
 //jawsdb
 module.exports = {
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
+  host: "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "bv36byf4elf4ylic",
+  password: "ksj049y94e828tdq",
   connectionLimit: 5,
-  database: "dd4me"
+  database: "u3yikfybcx1baovi"
   
 };
 
