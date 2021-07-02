088b851dfb8abe524821aa5870bec82a69689d13
Theo <theocaz@gmail.com>
2021-07-02 00:40:06 -0400

    update

--- dbConn/keys.js
+++ dbConn/keys.js
@@ -2,11 +2,11 @@
 //jawsdb
 module.exports = {
   host: "pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  host: "ec2-54-85-13-135.compute-1.amazonaws.com",
-  database: "ddrqovmbchidj0",
-  user: "dgrvwdjftroipm",
-  port: "5432",
-  password: "09e92314127997d08c751ab5a74c02c211e23171decc45e2aba7868e1572f013"
+  user: "gz9qosfn3mhqxce0",
+  password: "i66mmjkugrq2f84y",
+  connectionLimit: 5,
+  port: 3306,
+  database: "zlrk68waoei9ph5w"
   
 };
 
